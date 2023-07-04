5ec145c377bcd2488172e0a516a4d355e706ad81
Nigel Bosch <pnigelb@gmail.com>
2023-07-04 17:32:19 -0500

    Add v1 and example docs

---
+++ README.md
@@ -0,0 +1,33 @@
+# Indezoek: Simple text file indexing and search
+
+This is a very simple text indexing and search strategy using nothing but default Python libraries. The project was born from a need to select text documents from among hundreds of thousands, which is intractable to search with `find` and `grep`, and too large to work in a SQLite full-text search (FTS5) database.
+
+There are much fancier/better solutions like Elasticsearch or Solr, but those are more power (and setup time and resource requirements) than I needed.
+
+Indezoek is designed for fast indexing of a lot of documents, and search that requires a bit of patience. Queries for common words that appear in many documents might take up to a minute per gigabyte of documents. Queries for uncommon words are much faster.
+
+## Usage
+
+Requires Python 3.10+
+
+```bash
+python indezoek.py -h  # See help info
+```
+
+In a typical case you might want to find all documents of a particular type and index them, like this:
+
+```bash
+find . -type f -iname '*.txt' > files.list
+python indezoek.py test.db --add-file-list files.list
+python indezoek.py test.db --search chair fingers
+```
+
+If you're done adding files, it is also good to add an SQL index to the database. It is generally best to do that only at the end since, once indexed, inserting new documents will be much slower. For this reason, there is also a `--dbunindex` option to drop the index:
+
+```bash
+python indezoek.py test.db --dbindex  # Add SQL index
+```
+
+## Search terms
+
+Documents are matched only if all search terms are present. Search terms are case-insensitive if specified in lowercase; any term with a capital letter in it will be searched for in a case-sensitive fashion. Only letters a-z are allowable in search terms, though spaces are possible by putting quotes around a term, like `--search "the whale" boat chair`.
---
+++ indezoek.py
@@ -0,0 +1,121 @@
+from collections import Counter
+import argparse
+
+import sqlite3
+
+
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+SPACES = ' \n'
+
+
+def make_tables(sqlite_cursor: sqlite3.Cursor):
+    sqlite_cursor.execute('''CREATE TABLE IF NOT EXISTS docs (
+        doc_id INTEGER PRIMARY KEY,
+        path TEXT UNIQUE
+    );''')
+    sqlite_cursor.execute('''CREATE TABLE IF NOT EXISTS words (
+        idx INTEGER PRIMARY KEY,
+        word TEXT,
+        doc_id INTEGER,
+        count INTEGER,
+        FOREIGN KEY(doc_id) REFERENCES docs(doc_id)
+    );''')
+
+
+def index_doc(sqlite_cursor: sqlite3.Cursor, path: str):
+    with open(path, 'r', encoding='utf8', errors='ignore') as infile:
+        txt = ''.join([l for l in infile.read().lower() if l in ALPHABET or l in SPACES])
+    words = txt.split()
+    word_counts = Counter(words)
+    # Save to DB
+    sqlite_cursor.execute('INSERT INTO docs (path) VALUES (?);', (path,))
+    doc_id = sqlite_cursor.lastrowid
+    sqlite_cursor.executemany('INSERT INTO words (doc_id, word, count) VALUES (?, ?, ?)',
+                              [(doc_id, w, c) for w, c in word_counts.items()])
+
+
+def search_docs(sqlite_cursor: sqlite3.Cursor, terms: list[str], quiet: bool = False):
+    term_words = []
+    for term in terms:
+        term_words.extend(term.split())  # Split multi-word quoted terms into words
+    term_words = [w.lower() for w in term_words]
+    # Find docs with all of the words in each of the terms
+    doc_ids = set()
+    for i, word in enumerate(term_words):
+        if not quiet:
+            print('Searching for:', ' '.join(term_words[:i + 1]), end='\r')
+        sqlres = sqlite_cursor.execute('SELECT doc_id FROM words WHERE word = ?', (word,))
+        if i == 0:
+            doc_ids.update([str(row[0]) for row in sqlres])
+        else:
+            doc_ids.intersection_update([str(row[0]) for row in sqlres])
+    # This was a clever query to search all terms at once but makes it hard to see progress
+    # q = 'SELECT doc_id FROM words WHERE word IN ("' + '", "'.join(term_words) + \
+    #     '") GROUP BY doc_id HAVING COUNT(*) = ' + str(len(term_words)) + ';'
+    # doc_ids = [str(row[0]) for row in sqlite_cursor.execute(q)]
+    # Get paths of docs from result
+    if not quiet:
+        print('\nGetting document paths')
+    q = 'SELECT path FROM docs WHERE doc_id IN (' + ', '.join(doc_ids) + ');'
+    paths = [row[0] for row in sqlite_cursor.execute(q)]
+    paths_to_remove = set()
+    # Search resulting files if needed (complex terms)
+    if any(t not in term_words for t in terms):
+        for i, path in enumerate(paths):
+            if not quiet:
+                print('Full-text searching result', i + 1, '/', len(paths), end='\r')
+            with open(path, 'r', encoding='utf8', errors='ignore') as infile:
+                txt = ' ' + ' '.join(infile.read().split()) + ' '
+            txtlower = txt.lower()
+            for term in terms:
+                if term not in term_words:
+                    firstmatch = -1
+                    matchlen = len(term)  # Future-proofing terms with markup
+                    if term == term.lower():
+                        firstmatch = txtlower.find(term)
+                    else:
+                        firstmatch = txt.find(term)
+                    if firstmatch < 0 or txtlower[firstmatch - 1] in ALPHABET or \
+                            txtlower[firstmatch + matchlen] in ALPHABET:
+                        paths_to_remove.add(path)  # Not a match
+                        break
+        if not quiet:
+            print()
+    paths = sorted(set(paths).difference(paths_to_remove))
+    if len(paths):
+        print('\n'.join(paths))
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('sqlite_db', help='Path to SQLite DB file (need not exist)')
+    ap.add_argument('--add-file-list', metavar='PATH',
+                    help='Path to newline-delimited list of paths to add')
+    ap.add_argument('--search', metavar='TERM', nargs='+',
+                    help='Space-delimited list of search terms (ANDed together)')
+    ap.add_argument('--dbindex', action='store_true', help='Add SQL index to DB')
+    ap.add_argument('--dbunindex', action='store_true', help='Remove SQL index from DB')
+    ap.add_argument('--quiet', action='store_true', help='Output only results in search')
+    args = ap.parse_args()
+
+    sqlcon = sqlite3.connect(args.sqlite_db)
+    cursor = sqlcon.cursor()
+    make_tables(cursor)
+
+    if args.add_file_list:
+        with open(args.add_file_list, 'r', encoding='utf8') as infile:
+            paths = infile.readlines()
+        for i, path in enumerate(paths):
+            print('Indexing', i + 1, '/', len(paths), end='\r')
+            if len(path.rstrip('\n')):
+                index_doc(cursor, path.rstrip('\n'))
+        print('\nCommitting SQL transaction')
+        sqlcon.commit()
+    if args.dbindex:
+        print('Creating SQL word index')
+        cursor.execute('CREATE INDEX idx_word ON words (word);')
+    elif args.dbunindex:
+        print('Deleting SQL word index')
+        cursor.execute('DROP INDEX idx_word;')
+    if args.search:
+        search_docs(cursor, args.search, args.quiet)
